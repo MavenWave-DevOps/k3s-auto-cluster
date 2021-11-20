a9fb36db5458e50cf27af79e328272f3509027bc
tony-mw <81250444+tony-mw@users.noreply.github.com>
2021-11-20 11:30:25 -0700

    update

--- auto-k3s/config.go
+++ auto-k3s/config.go
@@ -1,7 +1,7 @@
 package autok3s
 
 const (
-	nodeQuantity  = 2
-	base_net      = "192.168.80"
+	nodeQuantity  = 4
+	base_net      = "192.168.86"
 	nodeTokenPath = "/var/lib/rancher/k3s/server/node-token"
 )
